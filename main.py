6eed1ac157fbc420dccc3f53513d1e058ac52029
erikakim005 <erikakim005@gmail.com>
2026-03-25 15:53:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from textblob import TextBlob
+import streamlit as st   # st= nickname / streamlit = library that build web apps only using python
+from textblob import TextBlob # instead of importing the whole texblob library, u just import the TextBlob class form it 
+# TextBlob = NLP tool that handles things like spell correction and sentimenr analysis
 
-text = input("Enter a sentence: ")
+st.title("✨ NLP Mini App") # display title in streamlit site 
 
-# correction
-corrected = TextBlob(text).correct()
+st.write("Type a sentence and click Analyze 👇") # st.write() = most basic way to display text on screen
 
-# sentiment
-sentiment = TextBlob(text).sentiment
+text = st.text_input("Enter a sentence:") # create text input box on the screen 
 
-print("Corrected:", corrected)
-print("Sentiment:", sentiment)
+analyze = st.button("Analyze") # when clicked = True / not clicked = False
+
+if analyze and text: # = the code will only run when the text is not empty!
+    blob = TextBlob(text) # variable blob will store the input text into a TextBlob object
+
+    #correction
+    corrected = str(blob.correct()) # str(blob.correct()) = TexBlob fix spelling and convert it to string. and sotre in variable corrected
+    
+    #sentiment
+    polarity = blob.sentiment.polarity # sentiment= contain emotional info / polarity = pulls out just the positive or negative score 
+    
+    if polarity > 0: 
+        sentiment_label = "Positive 😊"
+    elif polarity < 0:
+        sentiment_label = "Negative 😢"
+    else:
+        sentiment_label = "Neutral 😐"
+
+    # UI
+    st.subheader("Corrected Sentence") # st.subheader() = mediu sized heading 
+    st.success(corrected) # success message = text inside GREEN box 
+
+    st.subheader("Sentiment")
+    st.info(sentiment_label) # info mnassages = tect inside BLUE bow
